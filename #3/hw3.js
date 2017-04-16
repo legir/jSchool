4b007dc1459cb530f79cb13e61ac2dda480ecdd7
legir <zholudevoleg@gmail.com>
2017-04-16 15:03:47 +0300

    conflict fixes

--- #3/hw3.js
+++ #3/hw3.js
@@ -1,18 +1,8 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-module.exports = './hw3.js'
-=======
-=======
->>>>>>> 8832583b278216a08674db5d0075135b2c4c7ffd
 module.exports.map = map;
 module.exports.reduce = reduce;
 module.exports.every = every;
 module.exports.filter = filter;
 module.exports.sumMultiples = sumMultiples;
-<<<<<<< HEAD
->>>>>>> 8832583b278216a08674db5d0075135b2c4c7ffd
-=======
->>>>>>> 8832583b278216a08674db5d0075135b2c4c7ffd
 
 function map (arr, m) {
   let result = [];
@@ -38,7 +28,7 @@ function reduce (arr, f) {
       }
     }
   return result;
-};  
+};
 
 //____________________
 
